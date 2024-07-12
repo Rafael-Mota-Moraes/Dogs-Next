1fa7ae820b55799e8900d7a859ce4e4edbae823f
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-12 11:35:06 -0300

    Login Estilos

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -2,6 +2,11 @@
 import login from '@/app/actions/login';
 import { useFormState, useFormStatus } from 'react-dom';
 import Button from '@/components/forms/button';
+import Input from '@/components/forms/input';
+import ErrorMessage from '../helper/error-message';
+import React from 'react';
+import Link from 'next/link';
+import styles from './login-form.module.css';
 
 function FormButton() {
   const { pending } = useFormStatus();
@@ -23,14 +28,28 @@ export default function LoginForm() {
     error: '',
     data: null,
   });
+
+  React.useEffect(() => {
+    if (state.ok) window.location.href = '/conta';
+  }, [state.ok]);
   return (
     <>
-      <form action={action}>
-        <input type="text" name="username" placeholder="usuário" />
-        <input type="password" name="password" placeholder="senha" />
+      <form action={action} className={styles.form}>
+        <Input label="Usuário" name="username" type="text" />
+        <Input label="Senha" name="password" type="password" />
+        <ErrorMessage error={state.error} />
         <FormButton />
-        <p>{state.error}</p>
       </form>
+      <Link className={styles.perdeu} href={'/login/perdeu'}>
+        Perdeu a senha?
+      </Link>
+      <div className={styles.cadastro}>
+        <h2 className={styles.subtitle}>Cadastre-se</h2>
+        <p>Ainda não possui conta? Cadastre-se no site.</p>
+        <Link href="/login/criar" className="button">
+          Cadastro
+        </Link>
+      </div>
     </>
   );
 }
