3569680177d67c34fad77c31c69fd2416f485877
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-13 19:06:56 -0300

    Conta header

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,5 +1,4 @@
 'use client';
-import userGet from '@/actions/user-get';
 import { useUser } from '@/context/user-context';
 import React from 'react';
 
@@ -8,7 +7,7 @@ export default function ContaPage() {
 
   return (
     <main>
-      <h1 onClick={() => console.log('teste')}>Conta: {user?.nome}</h1>
+      <h1>Conta: {user?.nome}</h1>
     </main>
   );
 }
