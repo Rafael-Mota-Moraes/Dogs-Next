8d9858b5d68845fac608f3de7104b5d24cb2f860
Rafael Mota Moraes <rafaelmota622@gmail.com>
2024-07-13 13:53:30 -0300

    Create Context

--- src/actions/user-get.ts
+++ src/actions/user-get.ts
@@ -3,6 +3,7 @@
 import { USER_GET } from '@/functions/api';
 import apiError from '@/functions/api-error';
 import { cookies } from 'next/headers';
+// import { cache } from 'react';
 
 export type User = {
   id: number;
@@ -14,7 +15,7 @@ export type User = {
 export default async function userGet() {
   try {
     const token = cookies().get('token')?.value;
-    if (!token) throw new Error('Token não encontrado');
+    if (!token) throw new Error('Token não encontrado.');
     const { url } = USER_GET();
     const response = await fetch(url, {
       method: 'GET',
@@ -25,10 +26,13 @@ export default async function userGet() {
         revalidate: 60,
       },
     });
-    if (!response.ok) throw new Error('Erro ao buscar o usuário.');
+    if (!response.ok) throw new Error('Erro ao pegar o usuário.');
     const data = (await response.json()) as User;
-    return { data: data, ok: true, error: '' };
+    return { data, ok: true, error: '' };
   } catch (error: unknown) {
     return apiError(error);
   }
 }
+
+// const userGetCache = cache(userGet);
+// export default userGetCache;
--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,7 +1,14 @@
-export default async function ContaPage() {
+'use client';
+import userGet from '@/actions/user-get';
+import { useUser } from '@/context/user-context';
+import React from 'react';
+
+export default function ContaPage() {
+  const { user } = useUser();
+
   return (
     <main>
-      <h1>Conta</h1>
+      <h1 onClick={() => console.log('teste')}>Conta: {user?.nome}</h1>
     </main>
   );
 }
